a00a9b13385fe94b4b5c217e495ed2566c31a07f
farashaeian <z.farashaeian@gmail.com>
2023-11-25 14:47:44 +0330

    get data from server instead of data.js file

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Book from "./Book";
 import Footer from './Footer';
-import { getBooks } from '../data/data';
+ // get data from a js file  ith a function instead of locating the array directly here:
+// import { getBooks } from '../data/data';
+import axios from 'axios';
+
 
 const Books = () => {
 
     // get data from a js file  ith a function instead of locating the array directly here:
-    const initialBooksList = getBooks();
+    // const initialBooksList = getBooks();
+    // const [booksList, setBooksList] = useState(initialBooksList);
 
-    // const initialBooksList = books;
-
-    const [booksList, setBooksList] = useState(initialBooksList);
+    const [books, setBooks] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedBook, setSelectedBook] = useState(null);
     const [selectedLeftColumnBook, setSelectedLeftColumnBook] = useState(null);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data: booksData } = await axios.get(
+                    "http://localhost:9000/books"
+                );
+                setBooks(booksData);
+
+            } catch (err) {
+                console.log(err.message);
+            }
+        }
+        fetchData();
+    }, [])
+
 
     const showBook = (book) => {
         setSelectedBook(book);
@@ -26,7 +45,7 @@ const Books = () => {
     };
 
     // Filter books based on the search term
-    const filteredBooks = booksList.filter((book) =>
+    const filteredBooks = books.filter((book) =>
         searchTerm !== '' && book.Name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
@@ -39,7 +58,7 @@ const Books = () => {
             <div className='row'>
                 <div className='p-5 col-6'>
                     {/* Display books in the left column */}
-                    {booksList.map((item, index) => (
+                    {books.map((item, index) => (
                         <p key={index} onClick={() => showBook(item)}>
                             {item.Name}
                         </p>
